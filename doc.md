11ce9fea759282ed01ce4bbfd5b53ae93d5f596e
int <125262137+StructuredObjects@users.noreply.github.com>
2023-05-17 16:50:13 -0700

    Lib Re-write

---
+++ doc.md
@@ -0,0 +1,36 @@
+<div align="center">
+<h1> Yoworld Item API SDK </h1>
+
+### Introducing our API documentation!
+
+Here we host muliple endpoints for developers to create such yoworld tools and distribute them to the community
+
+# Item Search
+Endpoint: ``/search``
+GET Parameters: ``q``
+
+GET Request Example using cURL
+```
+curl -s 'https://api.yoworld.site/search?q=257460'
+```
+
+GET Request Example using Python
+```
+results = requests.get("https://api.yoworld.site/search?q=257460)
+print(results.text)
+```
+
+# Advance Item Information
+Endpoint: ``/advance``
+GET Parameters: ``q``
+
+GET Request Example using cURL
+```
+curl -s 'https://api.yoworld.site/advance?q=257460'
+```
+
+GET Request Example using Python
+```
+results = requests.get("https://api.yoworld.site/advanced?q=257460)
+print(results.text)
+```
---
+++ lib.py
@@ -0,0 +1,121 @@
+import requests, enum, json
+
+class API:
+    url                 = "https://api.yoworld.site"
+    searchEndpoint      = f"{url}/search"
+    infoEndpoint        = f"{url}/advance"
+    changeEndpoint      = f"{url}/change"
+
+class Results(enum.Enum):
+    NONE                = 0
+    EXACT_FOUND         = 1
+    EXTRA_FOUND         = 2
+
+class Item():
+    """ Main Item Information Needed """
+    name: str
+    id: int
+    url: str
+    price: str
+    update: str
+
+    """ Extra Item Information """
+    gender: str
+    is_free_gift: bool
+    is_tradable: bool
+    is_giftable: bool
+    is_instore: bool
+    category: str
+    xp: int
+    def __init__(self) -> None:
+        pass
+
+    def new_item(self, n: str, iid: int, u: str, p: str, last_u: str) -> None:
+        self.name = n; self.id = iid; self.url = u;
+        self.price = p; self.last_update = last_u;
+
+    def arr2item(self, arr: list) -> None:
+        if len(arr) != 5:
+            print(f"[ X ] Error, Unable to parse this list ( {arr} )");
+            return;
+    
+        self.name = arr[0]; 
+        self.id = arr[1]; 
+        self.url = arr[2];
+        self.price = arr[3]; 
+        self.last_update = arr[4];
+
+    def add_extra_info(self, arr: list) -> None:
+        if len(arr) != 7:
+            print(f"[ X ] Error, Unable to parse this list ( {arr} )");
+            return;
+    
+        self.gender = arr[0];
+        self.is_tradable = bool(int(arr[1]));
+        self.is_free_gift = bool(int(arr[2]));
+        self.is_giftable = bool(int(arr[3]));
+        self.is_instore = bool(int(arr[4]))
+        self.category = arr[5];
+        self.xp = arr[6];
+
+class YoworldSite:
+    @staticmethod
+    def ResultCheck(results: list) -> Results:
+        if results == []: return Results.NONE;
+        elif results == ['']: return Results.NONE;
+        elif results == 1: return Results.EXACT_FOUND;
+        elif results > 1: return Results.EXTRA_FOUND;
+
+    @staticmethod 
+    def searchItem(query: str) -> list:
+        if len(query) == 0: return [];
+        results = requests.get(f"{API.searchEndpoint}?q={query}")
+        if results.status_code != 200:
+            return []; ## RETURN NOTHING IF API IS DOWN
+
+        response = results.text;
+
+        if response == "[ X ] Error, You must fill all parameters to continue!": return [];
+        if "No items found" in response: return [];
+        if "\n" not in response: return [response]; ## DETECTED ONLY ONE ITEM! RETURN IT
+        
+        lines = response.split("\n");
+        found = [];
+
+        for line in lines:
+            if len(line) < 5: break;
+            info = line.replace("[", "").replace("]", "").split(",");
+            if len(info) == 5:
+                new_item = Item("", "", "", "", "");
+                new_item.arr2item(info)
+                found.append(new_item);
+            else:
+                print(f"[ X ] Error, Unable to parse this line!\n{line}");
+
+        return found;
+
+    @staticmethod
+    def infoSearch(item_id: str, itm: Item | None) -> Item:
+        if len(item_id) == 0 | item_id.isdigit() == False: return Item("", "", "", "", "");
+        result = requests.get(f"{API().infoEndpoint}?q={item_id}");
+        if result.status_code != 200:
+            return Item("", "", "", "", "");
+
+        response = result.text;
+
+        if not response.startswith("{") and not response.endswith("}"): return Item("", "", "", "", "");
+        if response == "[x] Error, Invalid Item ID.....!": return Item("", "", "", "", "");
+
+        json_obj = json.loads(response)
+        item_info = [];
+        for key in json_obj:
+            val = json_obj[key]
+            item_info.append(val)
+
+        if itm != None:
+            itm.add_extra_info(item_info);
+            return itm;
+
+        new_item = Item();
+        new_item.add_extra_info(item_info);
+        return new_item;
