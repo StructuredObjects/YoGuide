00e674698aaa043181940e5d2eaec3fb4881def2
Nefarious <c0deChieff@gmail.com>
2023-06-04 05:33:28 -0400

    gg

--- Side Tools/fix_prices.v
+++ Side Tools/fix_prices.v
@@ -1,5 +1,6 @@
 import os
-import vweb
+import net.http
+import x.json2 as j
 
 pub const db = "items.txt"
 
@@ -7,10 +8,37 @@ fn main()
 {
 	lines := os.read_lines(db) or { return }
 
+	mut new_db := ""
+
+	mut c := 0
 	for line in lines 
 	{
+		if line.len < 5 { continue }
+		info := parse(line)
 
+		req := http.post_form("https://yoworlddb.com/scripts/getItemInfo.php", {"iid": "${info[1]}"}) or { http.Response{} }
+		if req.body == "" { continue }
+
+		jobj := j.raw_decode(req.body) or { return }
+		info_obj := j.raw_decode("${jobj.as_map()['response']}") or { return }
+		item_info := info_obj.as_map()
+
+		in_store := "${item_info['active_in_store']}"
+		
+		if in_store == "1" {
+			mut price := "${item_info['price_coins']}c"
+
+			if price == "0" || price == "0c" { price = "${item_info['price_cash']}yc" }
+			new_db += "('${info[0]}','${info[1]}','${info[2]}','${price}','${info[4]}')"
+			println("[ + ] ${c}/${lines.len} New Item Priced! '${info[0]}' ${price}")
+		} else {
+			new_db += line
+			println("[ X ] ${c}/${lines.len} Item Skipped")
+		}
+		c++
 	}
+
+	os.write_file("testing.txt", new_db) or { return }
 }
 
 pub fn parse(line string) []string
---
+++ search.py
@@ -0,0 +1,27 @@
+import sys
+
+from api_lib import *
+
+args = sys.argv;
+
+if len(args) < 2:
+    print(f"[ X ] Error, Invalid argument provided\nUsage: {args[0]} <query>");
+    exit(0);
+
+query = f"{args}".replace("[", "").replace("]", "").replace("'", "").replace(",", "").replace(f"{args[0]} ", "").replace(f"{args[0]}", "");
+
+eng = YoworldEngine()
+n = eng.Search(query);
+if n == Response.NONE:
+    print("[ X ] Unable to find item.....!");
+    exit(0);
+elif n == Response.EXACT:
+    r = eng.getResults();
+    print(f"Item: {r[0].name} | {r[0].id} | {r[0].price} | {r[0].update}");
+    exit(0);
+elif n == Response.EXTRA:
+    r = eng.getResults();
+    c = 0
+    for itm in r:
+        print(f"Item: {r[c].name} | {r[c].id} | {r[c].price} | {r[c].update}");
+        c += 1
